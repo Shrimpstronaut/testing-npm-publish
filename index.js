c5c0436439a82aabb5595c629e2edfe8bc768bb6
Stefan Fiedler <sf@taikonauten.com>
2021-12-01 18:46:26 +0100

    added timo function

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ export function loremIpsum() {
     console.log('Lorem ipsum dolor sit amet');
 }
 
+export function timo() {
+    console.log('timo is the best!');
+}
+
 export default {
     helloWorld,
     loremIpsum
