f17c994380b63d11fbfbcf91edf83bfa095ece73
Stefan Fiedler <sf@taikonauten.com>
2021-12-01 12:57:16 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # testing-npm-publish
+
 A sample project used to test the automation of publishing NPM packages.
---
+++ index.js
@@ -0,0 +1,7 @@
+export function helloWorld() {
+    console.log('hello world');
+}
+
+export default {
+    helloWorld
+}
